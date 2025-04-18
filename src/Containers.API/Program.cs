dacb953683891bdca327523908800f9683d4f3fb
s31398 <s31398@pjwstk.edu.pl>
2025-04-18 10:52:54 +0200

    change settings

--- src/Containers.API/Program.cs
+++ src/Containers.API/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var connectionString = builder.Configuration.GetConnectionString("UniversityDatabase");
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
